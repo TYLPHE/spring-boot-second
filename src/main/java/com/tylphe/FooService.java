d96d4628126fdc89c80e25f6d672111342b9982c
tylphe <tylphet@gmail.com>
2023-12-25 15:48:57 -0700

    connecting to a postgres database on docker

--- src/main/java/com/tylphe/FooService.java
+++ src/main/java/com/tylphe/FooService.java
@@ -1,6 +1,12 @@
 package com.tylphe;
 
+import com.tylphe.customer.Customer;
+import com.tylphe.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FooService {
@@ -15,5 +21,26 @@ public class FooService {
 
     String getFooName() {
         return foo.name();
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer alex = new Customer(
+                    "alex",
+                    "alex@alex.com",
+                    21
+            );
+
+            Customer joe = new Customer(
+                    "joe",
+                    "joe@joe.com",
+                    21
+            );
+
+            List<Customer> customers = List.of(alex, joe);
+
+            customerRepository.saveAll(customers);
+        };
     }
 }
---
+++ src/main/java/com/tylphe/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,26 @@
+package com.tylphe.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jpa")
+public class CustomerJPADataAccessService implements CustomerDAO{
+
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return customerRepository.findById(id);
+    }
+}
